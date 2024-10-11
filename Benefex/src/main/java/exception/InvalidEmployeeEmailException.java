68b29d4451e07dfb63ae3efeb0cc17a5f471e74e
TitouanLab <titouan.labbe@hotmail.com>
2024-10-11 10:34:24 +0100

    Returning 400 error instead of 500

--- Benefex/src/main/java/exception/InvalidEmployeeEmailException.java
+++ Benefex/src/main/java/exception/InvalidEmployeeEmailException.java
@@ -1,5 +1,9 @@
 package exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class InvalidEmployeeEmailException extends RuntimeException {
     public InvalidEmployeeEmailException() {
         super("Invalid Employee Email format");
--- Benefex/src/main/java/exception/NotUniqueEmployeeEmailException.java
+++ Benefex/src/main/java/exception/NotUniqueEmployeeEmailException.java
@@ -1,5 +1,9 @@
 package exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 public class NotUniqueEmployeeEmailException extends RuntimeException {
     public NotUniqueEmployeeEmailException() {
         super("This employee has already been registered.");
