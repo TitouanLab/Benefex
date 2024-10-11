9916d7bd59fe3954945827813141cdc7e164f5df
TitouanLab <titouan.labbe@hotmail.com>
2024-10-11 08:43:24 +0100

    Adding test for email error

---
+++ Benefex/src/test/java/com/springboot/benefex/benefexproject/validator/EmployeeValidatorTest.java
@@ -0,0 +1,36 @@
+package com.springboot.benefex.benefexproject.validator;
+
+import com.springboot.benefex.benefexproject.dto.EmployeeRequest;
+import exception.EmployeeEmailNullOrEmpty;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class EmployeeValidatorTest {
+
+    @Autowired
+    private EmployeeValidator employeeValidator;
+
+    @Test
+    public void validateEmail_validRequest_noException() {
+        EmployeeRequest employeeRequest = new EmployeeRequest("Mr", "John", "Smith", LocalDate.of(1900, 1, 1), "Male", "john.smith@gmail.com", "1 Somewhere Road");
+        employeeValidator.validateEmployeeRequest(employeeRequest);
+    }
+
+    @Test
+    public void validateEmail_nullEmail_throwsNullNameException() {
+        EmployeeRequest employeeRequest = new EmployeeRequest("Mr", "John", "Smith", LocalDate.of(1900, 1, 1), "Male", null, "1 Somewhere Road");
+        assertThrows(EmployeeEmailNullOrEmpty.class, () -> employeeValidator.validateEmployeeRequest(employeeRequest));
+    }
+
+    @Test
+    public void validateEmail_emptyEmail_throwsNullNameException() {
+        EmployeeRequest employeeRequest = new EmployeeRequest("Mr", "John", "Smith", LocalDate.of(1900, 1, 1), "Male", "", "1 Somewhere Road");
+        assertThrows(EmployeeEmailNullOrEmpty.class, () -> employeeValidator.validateEmployeeRequest(employeeRequest));
+    }
+}
