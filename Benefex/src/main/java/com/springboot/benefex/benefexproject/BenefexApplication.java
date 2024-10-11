e2d159ed1ce808a8add6c350b154f34e75aeef3c
TitouanLab <titouan.labbe@hotmail.com>
2024-10-11 07:46:18 +0100

    Project creation

---
+++ Benefex/src/main/java/com/springboot/benefex/benefexproject/BenefexApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.benefex.benefexproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BenefexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BenefexApplication.class, args);
+    }
+
+}
